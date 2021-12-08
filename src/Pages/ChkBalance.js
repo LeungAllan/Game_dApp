5da6db7d04e59c77f52931910f534906c73fbda5
Allan Leung <42834824+LeungAllan@users.noreply.github.com>
2021-12-08 16:19:04 +0800

    initial commit

--- src/Pages/ChkBalance.js
+++ src/Pages/ChkBalance.js
@@ -1,19 +1,31 @@
-import { useEtherBalance, useEthers } from "@usedapp/core";
-import { formatEther } from "@ethersproject/units";
+import { useEtherBalance, useEthers, useTokenBalance } from "@usedapp/core";
+import { formatEther, formatUnits } from "@ethersproject/units";
 
 const Balance = () => {
   const { account, chainId } = useEthers();
   const etherBalance = useEtherBalance(account);
+  const NFTBalance = useTokenBalance(
+    "0x748F4188BC18348315c423F03E065cfDBAef3b44",
+    account
+  );
 
   return (
-    <div >
+    <div>
       <h1> Balance Page</h1>
       <h2>Show connected wallet ETH / Matic Balance</h2>
 
       <div>
         {account && <p>Account: {account}</p>}
         {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
+        <p>HeroXYZ Balance: {NFTBalance ? formatUnits(NFTBalance, 6) : 0}</p>
         <p> Chain ID : {chainId}</p>
+        {chainId !== 4 ? (
+          <p class="errorText">
+            We support Rinkeby network only, please change your network!
+          </p>
+        ) : (
+          <p></p>
+        )}
       </div>
     </div>
   );
