f21666faaf3aabbde2fc44624865561ec751178e
Allan <allan@Allans-MacBook-Pro.local>
2021-12-09 23:35:14 +0800

    update v2

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,7 @@ const About = () => {
     return (
         <div >
             <h1>About Us</h1>
-            <h2>We are crazy</h2>
+            <p>Kai Tech Labs is formed by a group of Anime Fans and Collectible Toy Manufacturers in 2021. With our 18 years of experience in the industry, we are strong in designing and making anime characters in 2D and 3D. Hero MIX is our first project and the beginning of our crypto journey.y</p>
         </div>
         
     )
