02353f546eca35f1a719031f683b71072aa54479
Allan <allan@Allans-MacBook-Pro.local>
2021-12-10 00:34:47 +0800

    Ver 2

--- src/Pages/ChkBalance.js
+++ src/Pages/ChkBalance.js
@@ -1,9 +1,11 @@
-import { useEtherBalance, useEthers } from "@usedapp/core";
-import { formatEther } from "@ethersproject/units";
+import { useEtherBalance, useEthers, useTokenBalance } from "@usedapp/core";
+import { formatEther, formatUnits } from "@ethersproject/units";
 
 const Balance = () => {
   const { account, chainId } = useEthers();
   const etherBalance = useEtherBalance(account);
+  const SmileBalance = useTokenBalance('0x65254Be8519e97DB783d0FfDE6646E104c5277a5', account)/100000000000;
+
 
   return (
     <div >
@@ -12,8 +14,12 @@ const Balance = () => {
 
       <div>
         {account && <p>Account: {account}</p>}
-        {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
-        <p> Chain ID : {chainId}</p>
+        <h3>Ethers: {etherBalance ? formatEther(etherBalance) : 0 } {(chainId===1 || chainId===4 )&& "ETH"}{chainId===137 && "MATIC"}</h3>
+        <h3>Smiles: {SmileBalance ? formatUnits(SmileBalance, 6).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : 0 }</h3>
+        {chainId!==4 && <font class="errortext">Please change to use Rinkeby</font>}
+        
+        
+
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import Header from "./Pages/Header";
 import { DAppProvider, ChainId, Config } from "@usedapp/core";
 
 const config: Config = {
-  readOnlyChainId: ChainId.Mainnet,
-  readOnlyUrls: {
-    [ChainId.Mainnet]:
-      "https://mainnet.infura.io/v3/62687d1a985d4508b2b7a24827551934"
-  }
+  readOnlyChainId: ChainId.Rinkeby,
+  //readOnlyUrls: {
+  //  [ChainId.Mainnet]:
+  //    "https://mainnet.infura.io/v3/62687d1a985d4508b2b7a24827551934"
+  //}
 };
 
 ReactDOM.render(
