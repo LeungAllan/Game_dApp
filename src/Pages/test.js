16837a5ac9d1575af3a3ae47a410f957fbb1168c
Allan Leung <42834824+LeungAllan@users.noreply.github.com>
2021-12-17 17:49:16 +0800

    Update test.js
    Replace error due to tokenIDs is not yet obtain data from useContractCalls.

--- src/Pages/test.js
+++ src/Pages/test.js
@@ -1,18 +1,18 @@
-import {  utils, BigNumber } from "ethers";
+import { utils } from "ethers";
 import { useEthers, useContractCalls } from "@usedapp/core";
-import react,{ useState, Component } from "react";
+import { useState, useEffect, useCallback } from "react";
 import abiJSON from "../Contract/NFTContract.json";
 import classes from "./Page.module.css";
 
 const abi = new utils.Interface(abiJSON);
 
 const Testapp = () => {
-  const [ address ] = useState("0x412364A058d8A7D33517D312A78b3de2174601c0");
+  const [address] = useState("0x412364A058d8A7D33517D312A78b3de2174601c0");
   const { account } = useEthers();
 
   // 可以用 useContractCalls 來打包
-  
- const [tokenIDs] = useContractCalls([
+
+  const [tokenIDs] = useContractCalls([
     {
       abi,
       address,
@@ -20,43 +20,41 @@ const Testapp = () => {
       args: [account]
     }
   ]);
-  function hex_to_ascii(str1)
-  {
-   var hex  = str1.toString();
-   var str = '';
-   for (var n = 0; n < hex.length; n += 2) {
-     str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
-   }
-   return str;
-  }
-  function replacer(key, value) {
-    if(typeof key === "hex") {
-      return value;
+
+  const testdata = JSON.stringify(tokenIDs, ["hex"]);
+  const { state, setState } = useState(testdata);
+  const DataAry2 = [];
+  /*
+  const transformdata = useCallback(() => {
+    if (testdata !== "") {
+      console.log(testdata);
+      const data = testdata
+        .replace(/{"hex":/g, "")
+        .replace(/}/g, "")
+        .replace(/\[\[/g, "")
+        .replace(/\]\]/g, "");
+
+      const DataAry = data.split(",");
+      DataAry2.push(
+        DataAry.map((item) => {
+          return parseInt(item.substring(3, item.length - 1), 16);
+        })
+      );
     }
-    //const newvalue = value.replace(/{"hex":/g,'').replace(/}/g,'').replace(/\[\[/g,'[').replace(/\]\]/g,']');
-    //const newvalue2 = hex_to_ascii(newvalue);
-    return undefined;
-  };
+  }, [testdata, DataAry2]);
 
-  //const data = JSON.stringify(tokenIDs,['hex'])
-  const data = JSON.stringify(tokenIDs,['hex']).replace(/{"hex":/g,'').replace(/}/g,'').replace(/\[\[/g,'').replace(/\]\]/g,'');
-  //const data2 = JSON.stringify(tokenIDs, replacer());
-
-  const DataAry = data.split(',');
-  const DataAry2 = DataAry.map((item) => {
-    return parseInt( item.substring(3,item.length-1),16);
-  });
-  
-
+  useEffect(() => {
+    transformdata();
+  }, [transformdata]);
+*/
   return (
-  <div className={classes.NFTList}>
-      <p>Data: {data}</p>
+    <div className={classes.NFTList}>
+      <p>Data: {testdata}</p>
       <p>====</p>
-      
-      <p> Data ARy in Json : {JSON.stringify(DataAry2)}</p>
-      <p>====</p>
-      <p>{JSON.stringify(tokenIDs)}</p></div>);
-
+      <p> Data ARy in Json : {DataAry2}</p>: <p> No Record </p>}<p>====</p>
+      <p>{JSON.stringify(tokenIDs)}</p>
+    </div>
+  );
 };
 
 export default Testapp;
