6c19b9f2e15ca4519c36dafc9ceed057cf2093e4
Allan Leung <42834824+LeungAllan@users.noreply.github.com>
2021-12-22 15:02:21 +0800

    Use new method for NFTlist

--- src/Pages/NFTList.js
+++ src/Pages/NFTList.js
@@ -9,7 +9,7 @@ const abi = new utils.Interface(abiJSON);
 const NFTList = () => {
   const [address] = useState("0x412364A058d8A7D33517D312A78b3de2174601c0");
   const { account } = useEthers();
-  const [ Arystate, setAryState ] = useState(null);
+  const [Arystate, setAryState] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -27,8 +27,9 @@ const NFTList = () => {
   const transformdata = useCallback(() => {
     setIsLoading(true);
     setError(null);
-    
+
     try {
+      /*
       if (JSON.stringify(tokenIDs, ["hex"]) !== "") {
         const data = JSON.stringify(tokenIDs, ["hex"])
           .replace(/{"hex":/g, "")
@@ -44,90 +45,100 @@ const NFTList = () => {
             return parseInt(item.substring(3, item.length - 1), 16);
           })
         );
+        */
+      const DataAry2 = [];
+      DataAry2.push(
+        tokenIDs.map((item) => {
+          return item * 1;
+        })
+      );
 
-        setAryState(JSON.stringify(DataAry2));
-    }} catch (error){
+      setAryState(JSON.stringify(DataAry2));
+    } catch (error) {
       setError(error.message);
-    };
+    }
     setIsLoading(false);
-  },[setAryState, tokenIDs]);
+  }, [setAryState, tokenIDs]);
 
   // Sample tokenID for test only
   //const [tokenID] = useState([1,2]);
   const [NFTlist, setNFTlist] = useState([]);
 
-  const getMetaData = useCallback( () => {
+  const getMetaData = useCallback(() => {
     setIsLoading(true);
     setError(null);
     let tokenID = [];
-  
-    if (Arystate!==null) {
+
+    if (Arystate !== null) {
       tokenID = Arystate.replace(/\[\[/g, "").replace(/\]\]/g, "").split(",");
-  
+
       const tokenURI = tokenID.map((number) => {
-      //  const tokenURI = Arystate.map((number) => {
-        const id ="0000" + number;
+        //  const tokenURI = Arystate.map((number) => {
+        const id = "0000" + number;
         return (
-        "https://gateway.pinata.cloud/ipfs/QmSxEKQTMX7xYXEVb84L9vgY13Wvy6UTPT2VsKpdJdzShj/HEROMIX_" + id.substr(id.length - 4 ) + ".json"
+          "https://gateway.pinata.cloud/ipfs/QmSxEKQTMX7xYXEVb84L9vgY13Wvy6UTPT2VsKpdJdzShj/HEROMIX_" +
+          id.substr(id.length - 4) +
+          ".json"
         );
       });
 
       const loadedMeta = [];
 
       tokenURI.map(async (token) => {
-      try {
-        /*
+        try {
+          /*
         const response = await fetch(
           "https://gateway.pinata.cloud/ipfs/QmSxEKQTMX7xYXEVb84L9vgY13Wvy6UTPT2VsKpdJdzShj/HEROMIX_0001.json"
         );
         */
-        
+
           const response = await fetch(token);
-      
-        if (!response.ok) {
-          throw new Error("Something went wrong!");
-        }
-        const data = await response.json();
-      
-        for (const key in data) {
-          if (key === 'description') {
-            loadedMeta.push({
-              id: key,
-              name: data.name,
-              description: data.description,
-              image: data.image
-            });
+
+          if (!response.ok) {
+            throw new Error("Something went wrong!");
           }
+          const data = await response.json();
+
+          for (const key in data) {
+            if (key === "description") {
+              loadedMeta.push({
+                id: key,
+                name: data.name,
+                description: data.description,
+                image: data.image
+              });
+            }
+          }
+
+          const uniqueloadedMeta = loadedMeta.filter((name, index) => {
+            const _record = JSON.stringify(name);
+            return (
+              index ===
+              loadedMeta.findIndex((obj) => {
+                return JSON.stringify(obj) === _record;
+              })
+            );
+          });
+
+          setNFTlist(uniqueloadedMeta);
+        } catch (error) {
+          setError(error.message);
         }
-       
-        const uniqueloadedMeta = loadedMeta.filter((name,index) => {
-          const _record = JSON.stringify(name);
-          return index === loadedMeta.findIndex( obj => {
-            return JSON.stringify(obj) === _record;
-          })
-        });
-
-        setNFTlist(uniqueloadedMeta);
-      
-      } catch (error) {
-        setError(error.message);
-      }
+      });
     }
-  )};
     setIsLoading(false);
-  //}, [tokenID]);
-}, [Arystate]);
-
+    //}, [tokenID]);
+  }, [Arystate]);
 
   useEffect(() => {
-      transformdata();
-      getMetaData();
+    transformdata();
+    getMetaData();
   }, [getMetaData, transformdata, Arystate]);
 
   let content = "";
 
   if (NFTlist.length > 0) {
-      content = <NFTCard nfts={NFTlist } />;
+    content = <NFTCard nfts={NFTlist} />;
   }
 
   if (error) {
